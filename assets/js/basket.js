7cc69b9cb90fff12a8a67d9656ac716458311777
Orkhansha <orkhansha@code.edu.az>
2022-09-06 02:40:27 +0400

    Added last changes

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,70 +1,45 @@
 let products = [];
 
 if (localStorage.getItem("products") != null) {
-    products = JSON.parse(localStorage.getItem("products"))
+  products = JSON.parse(localStorage.getItem("products"));
 }
 
 let tableBody = document.querySelector(".table .table-body");
 
-
- 
-
 for (const product of products) {
-    
-    
-
-    tableBody.innerHTML += `<tr>
+  tableBody.innerHTML += `<tr>
     <td><img src="${product.image}" height="100px" width="100px" class="card-img-top" alt="..."></td>
     <td>${product.name}</td>
     <td>${product.desc}</td>
     <td>${product.count}</td>
     <td class="icone"><i class="fa-solid fa-trash"></i></td>
-  </tr>`
-
- // <i class="fa-solid fa-trash"></i>
-
-
-  
+  </tr>`;
 }
-
 
 let deleteIcon = document.querySelectorAll(".icone");
 
-   
-deleteIcon.forEach(element => {
-    element.addEventListener("click",function(){
-        this.parentElement.remove() 
-        document.querySelector("sup").innerText=products.length
-        console.log(products.length)
-
-    })
-       
+deleteIcon.forEach((element) => {
+  element.addEventListener("click", function () {
+    this.parentElement.remove();
+    localStorage.removeItem("products");
+    document.querySelector("sup").innerText = products.length -= 1;
+  });
 });
 document.querySelector("sup").innerText = getProductsCount(products);
 
-function getProductsCount(items){
-    let resultCount = 0;
-    for (const item of items) {
-        resultCount += item.count
-    }
-    return resultCount;
+function getProductsCount(items) {
+  let resultCount = 0;
+  for (const item of items) {
+    resultCount += item.count;
+  }
+  return resultCount;
 }
 
-
-// let icon = document.createElement("i");
-
-// icon.classList.add("fa-solid");
-
-// icon.classList.add("fa-circle-xmark");
-
-// icon.style.float = "right";
-
-// icon.style.color = "red";
-
-// icon.style.cursor = "pointer"
-
-// li.append(icon);
-
-// icon.onclick = function(){
-//   li.remove();
-// }
+// document.querySelector("sup").innerText = getDeleteCount(products);
+// function getDeleteCount(items) {
+//     let deleteCount = 0;
+//     for (const item of items) {
+//         deleteCount = getProductsCount(products) - item.count;
+//     }
+//     return deleteCount;
+//   }
