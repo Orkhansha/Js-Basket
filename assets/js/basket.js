356b6a768f3a2ce46685be198ed937f48666c6bd
Orkhan Sh. Akbarov <orkhansha@code.local>
2022-09-05 23:25:03 -0700

    Added Js-Basket-last files

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -8,7 +8,7 @@ let tableBody = document.querySelector(".table .table-body");
 
 for (const product of products) {
   tableBody.innerHTML += `<tr>
-    <td><img src="${product.image}" height="100px" width="100px" class="card-img-top" alt="..."></td>
+    <td data-id="${product.id}"><img src="${product.image}" height="100px" width="100px" class="card-img-top" alt="..."></td>
     <td>${product.name}</td>
     <td>${product.desc}</td>
     <td>${product.count}</td>
@@ -20,9 +20,12 @@ let deleteIcon = document.querySelectorAll(".icone");
 
 deleteIcon.forEach((element) => {
   element.addEventListener("click", function () {
-    this.parentElement.remove();
-    localStorage.removeItem("products");
-    document.querySelector("sup").innerText = products.length -= 1;
+    let id = parseInt(this.parentNode.firstElementChild.getAttribute("data-id"));
+    products = products.filter(m=>m.id != id);
+    localStorage.setItem("products", JSON.stringify(products));
+    this.parentNode.remove();
+    document.querySelector("sup").innerText = getProductsCount(products);
+    
   });
 });
 document.querySelector("sup").innerText = getProductsCount(products);
